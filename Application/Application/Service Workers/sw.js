e703bf41869900289288faa25a2d0ec4a220d16b
ReinerLau <lk850593913@gmail.com>
2025-11-15 20:07:12 +0800

    Add Service Workers implementation with app.js, index.html, and sw.js files

---
+++ Application/Application/Service Workers/sw.js
@@ -0,0 +1,38 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll(["./app.js"]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.registration.navigationPreload.enable());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const preloadResponse = await event.preloadResponse;
+      if (preloadResponse) {
+        return preloadResponse;
+      }
+      const cache = await caches.open("v1");
+      const cachedResponse = await cache.match(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      const response = await fetch(event.request);
+      return response;
+    })()
+  );
+});
+
+self.addEventListener("push", (event) => {
+  const title = "Push Notification";
+  const body = event.data.text();
+  const options = {
+    body,
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
